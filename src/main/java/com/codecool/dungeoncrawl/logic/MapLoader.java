0c11feea3ca11d5510d55c0e49d8d722e25aca1f
LordBrutal <bogar.jozsef@gmail.com>
2021-11-02 16:42:56 +0100

    Add: new cases for different Items

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -2,6 +2,9 @@ package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.Skeleton;
+import com.codecool.dungeoncrawl.logic.items.Key;
+import com.codecool.dungeoncrawl.logic.items.Shield;
+import com.codecool.dungeoncrawl.logic.items.Sword;
 
 import java.io.InputStream;
 import java.util.Scanner;
@@ -38,6 +41,18 @@ public class MapLoader {
                         case '@':
                             cell.setType(CellType.FLOOR);
                             map.setPlayer(new Player(cell));
+                            break;
+                        case 'S':
+                            cell.setType(CellType.FLOOR);
+                            new Sword(cell);
+                            break;
+                        case 'H':
+                            cell.setType(CellType.FLOOR);
+                            new Shield(cell);
+                            break;
+                        case 'B':
+                            cell.setType(CellType.FLOOR);
+                            new Key(cell, "blue");
                             break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
