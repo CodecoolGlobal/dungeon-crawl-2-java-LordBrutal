8df329948f005c58dd61ba339636f5ad39fa92f7
LordBrutal <bogar.jozsef@gmail.com>
2021-11-03 14:08:18 +0100

    Add: inventory field and addToInventory() to Player

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -2,12 +2,16 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.Potion;
+import com.codecool.dungeoncrawl.logic.items.Shield;
+import com.codecool.dungeoncrawl.logic.items.Sword;
 
 import java.util.ArrayList;
 
 public class Player extends Actor {
 
     private ArrayList<Item> inventory = new ArrayList<>();
+    private int defense = 0;
 
     public Player(Cell cell) {
         super(cell);
@@ -26,10 +30,25 @@ public class Player extends Actor {
     public void addToInventory() {
         Item item = this.getCell().getItem();
         if (item != null) {
+            String itemClass = item.getClass().getSimpleName();
+            switch (itemClass) {
+                case "Sword":
+                    this.attack += ((Sword)item).getAttackModifier();
+                    break;
+                case "Shield":
+                    defense += ((Shield) item).getDefenseModifier();
+                    break;
+                case "Potion":
+                    setHealth(this.getHealth()+((Potion) item).getHealthIncrease());
+                    break;
+            }
             inventory.add(item);
             System.out.println(inventory);
             this.getCell().setItem(null);
         }
     }
 
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
 }
