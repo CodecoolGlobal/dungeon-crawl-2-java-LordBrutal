5e4d93d9a5b666fa0aab2ed21a1820e04883ff80
zoli0324 <zoli0324@gmail.com>
2021-11-04 12:05:33 +0100

    Modify: shield  value to 2 from 5

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Shield.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Shield.java
@@ -4,7 +4,7 @@ import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Shield extends Item{
 
-    private final int defenseModifier = 10;
+    private final int defenseModifier = 2;
 
     public Shield(Cell cell) {
         super(cell);
