9ac8f39f61cff0216c385f8a57f8ae545ee6afce
zsomborszego <szego.zsombor@gmail.com>
2021-11-16 16:14:47 +0100

    Removed: Player from GameState

--- src/main/java/com/codecool/dungeoncrawl/model/GameState.java
+++ src/main/java/com/codecool/dungeoncrawl/model/GameState.java
@@ -1,26 +1,25 @@
 package com.codecool.dungeoncrawl.model;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.sql.Date;
 import java.util.List;
 
 public class GameState extends BaseModel {
-    private Date savedAt;
+    private Timestamp savedAt;
     private String currentMap;
     private List<String> discoveredMaps = new ArrayList<>();
-    private PlayerModel player;
 
-    public GameState(String currentMap, Date savedAt, PlayerModel player) {
+    public GameState(String currentMap, Timestamp savedAt) {
         this.currentMap = currentMap;
         this.savedAt = savedAt;
-        this.player = player;
     }
 
-    public Date getSavedAt() {
+    public Timestamp getSavedAt() {
         return savedAt;
     }
 
-    public void setSavedAt(Date savedAt) {
+    public void setSavedAt(Timestamp savedAt) {
         this.savedAt = savedAt;
     }
 
@@ -38,13 +37,5 @@ public class GameState extends BaseModel {
 
     public void addDiscoveredMap(String map) {
         this.discoveredMaps.add(map);
-    }
-
-    public PlayerModel getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(PlayerModel player) {
-        this.player = player;
     }
 }
