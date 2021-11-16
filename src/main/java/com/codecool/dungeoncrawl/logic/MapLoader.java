583c6b2a65b06c14b84bb8db8332db1d64f93a49
LordBrutal <bogar.jozsef@gmail.com>
2021-11-16 13:14:45 +0100

    Modify: GameMap constructor call in MapLoader

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.logic;
 import com.codecool.dungeoncrawl.logic.actors.enemys.Cyclops;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.actors.enemys.Skeleton;
+import com.codecool.dungeoncrawl.logic.create_map_components.GenerateItems;
 import com.codecool.dungeoncrawl.logic.items.*;
 import com.codecool.dungeoncrawl.logic.actors.enemys.Spider;
 
@@ -18,7 +19,7 @@ public class MapLoader {
         int height = scanner.nextInt();
         scanner.nextLine(); // empty line
 
-        GameMap map = new GameMap(width, height, CellType.EMPTY);
+        GameMap map = new GameMap(width, height, level, CellType.EMPTY);
         for (int y = 0; y < height; y++) {
             String line = scanner.nextLine();
             for (int x = 0; x < width; x++) {
@@ -34,21 +35,9 @@ public class MapLoader {
                         case '.':
                             cell.setType(CellType.FLOOR);
                             break;
-                        case 's':
-                            cell.setType(CellType.FLOOR);
-                            map.setEnemys(new Skeleton(cell));
-                            break;
                         case '@':
                             cell.setType(CellType.FLOOR);
                             map.setPlayer(new Player(cell));
-                            break;
-                        case 'c':
-                            cell.setType(CellType.FLOOR);
-                            map.setEnemys(new Cyclops(cell));
-                            break;
-                        case 'w':
-                            cell.setType(CellType.FLOOR);
-                            map.setEnemys(new Spider(cell));
                             break;
                         case 'x':
                             cell.setType(CellType.BREAKABLEWALL);
@@ -68,47 +57,13 @@ public class MapLoader {
                         case 'D':
                             cell.setType(CellType.DOOR);
                             break;
-                        case 'B':
-                            cell.setType(CellType.FLOOR);
-                            new Key(cell, Color.BLUE.getColor());
-                            break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
                     }
                 }
             }
         }
-        generateItems(map);
+        GenerateItems.generateItems(map);
         return map;
-    }
-
-    private static void generateItems(GameMap map) {
-        String[] items = { "Potion", "PickAxe", "Shield", "Sword"};
-        for(String item: items) {
-            while (true) {
-                Random random1 = new Random();
-                int h = random1.nextInt(map.getHeight());
-                Random random2 = new Random();
-                int w = random2.nextInt(map.getWidth());
-                Cell cell = map.getCell(w, h);
-                if (cell.getType().equals(CellType.FLOOR) && cell.getActor() == null && cell.getItem() == null) {
-                    switch (item) {
-                        case "Potion":
-                            new Potion(cell);
-                            break;
-                        case "PickAxe":
-                            new PickAxe(cell);
-                            break;
-                        case "Shield":
-                            new Shield(cell);
-                            break;
-                        case "Sword":
-                            new Sword(cell);
-                            break;
-                    }
-                    break;
-                }
-            }
-        }
     }
 }
