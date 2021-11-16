adcc5d21d26533cb52272d58046825fec32e3e43
zoli0324 <zoli0324@gmail.com>
2021-11-16 12:01:25 +0100

    Add: extend with columns the add() method in PlayerDaoJdbc

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -16,12 +16,14 @@ public class PlayerDaoJdbc implements PlayerDao {
     @Override
     public void add(PlayerModel player) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "INSERT INTO player (player_name, hp, x, y) VALUES (?, ?, ?, ?)";
+            String sql = "INSERT INTO player (player_name, hp, def,  x, y, items) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, player.getPlayerName());
             statement.setInt(2, player.getHp());
-            statement.setInt(3, player.getX());
-            statement.setInt(4, player.getY());
+            statement.setInt(3, player.getDef());
+            statement.setInt(4, player.getX());
+            statement.setInt(5, player.getY());
+            statement.setString(6, player.getItems());
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             resultSet.next();
