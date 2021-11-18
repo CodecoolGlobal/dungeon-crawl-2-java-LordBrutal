0bf0d359e6dab0b33ae06f7e88a1ca6a8db69327
predavies <iamosaretin@gmail.com>
2021-11-18 13:23:44 +0100

    test: remove wall size methods

--- src/test/java/com/codecool/dungeoncrawl/logic/actor_test/enemies/EnemyTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/actor_test/enemies/EnemyTest.java
@@ -2,24 +2,30 @@ package com.codecool.dungeoncrawl.logic.actor_test.enemies;
 
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.GameMap;
-import com.codecool.dungeoncrawl.logic.actors.enemys.Enemy;
-import com.codecool.dungeoncrawl.logic.actors.enemys.Skeleton;
-import com.codecool.dungeoncrawl.logic.create_map_components.GenerateItems;
-import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.actors.enemys.Cyclops;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EnemyTest {
 
     @Test
-    public void testRemoveWalls() {
-        GameMap gameMap = new GameMap(5,5,1, CellType.FLOOR);
-        Skeleton skeleton = new Skeleton(gameMap.getCell(3, 3));
-        // I want to know if the remove walls method includes a wall.
-        //what does this method actually do?
+    public void testRemoveWall0() {
+        GameMap gameMap = new GameMap(5,5,1, CellType.WALL);
+        gameMap.getCell(3, 3).setType(CellType.FLOOR);
+        Cyclops cyclops = new Cyclops(gameMap.getCell(3, 3));
+        assertEquals(0, cyclops.removeWalls().size());
+
+    }
+
+    @Test
+    public void testRemoveWallOneSize() {
+        GameMap gameMap = new GameMap(5,5,1, CellType.WALL);
+        gameMap.getCell(3, 3).setType(CellType.FLOOR);
+        gameMap.getCell(3,2).setType(CellType.FLOOR);
+        Cyclops cyclops = new Cyclops(gameMap.getCell(3, 3));
+
+        assertEquals(1, cyclops.removeWalls().size());
+
     }
 }
